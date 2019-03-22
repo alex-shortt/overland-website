db1703c4c3c023800e097e591039b42b784e3025
Alex Shortt <alexander.shortt@gmail.com>
2019-03-21 20:55:41 -0700

    clean useless shit

--- src/components/container.js
+++
@@ -1,9 +0,0 @@
-class Container {
-  constructor(element, x, y) {
-    this.elem = element
-    this.x = x
-    this.y = y
-  }
-}
-
-export default Container
--- src/index.js
+++ src/index.js
@@ -1,38 +1,8 @@
 import "./styles/main.scss"
-import Sphere from "./components/sphere.js"
-import Skybox from "./components/skybox.js"
 
 
-async function init(xcells, ycells) {
-  var skybox = new Skybox()
-  var sphere = new Sphere(xcells, ycells, skybox)
-  sphere.loadContainers()
-  sphere.initialOrient()
-
-  $(document).keydown(async function(e) {
-    switch (e.keyCode) {
-      case 37:
-        await sphere.zoomOut()
-        sphere.currX--
-        await sphere.moveTo(sphere.currX, sphere.currY)
-        break
-      case 39:
-        sphere.zoomOut()
-        sphere.currX++
-        sphere.moveTo(sphere.currX, sphere.currY)
-        break
-      case 38:
-        sphere.zoomOut()
-        sphere.currY++
-        sphere.moveTo(sphere.currX, sphere.currY)
-        break
-      case 40:
-        sphere.zoomOut()
-        sphere.currY--
-        sphere.moveTo(sphere.currX, sphere.currY)
-        break
-    }
-  })
+async function init() {
+  console.log("hello")
 }
 
-init(9, 9)
+init()
