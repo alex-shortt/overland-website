99439917b22d946b0b0944a6be739de323b312fd
Alex Shortt <alexander.shortt@gmail.com>
2019-04-02 16:57:46 -0700

    remove local jquery import, rough contact page

--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
-import "./styles/main.scss";
-import "./components/jquery-3.3.1.min.js";
-import "./components/jquery.ripples.js";
+import "./styles/main.scss"
+import "./components/jquery.ripples.js"
 
 // GLOBAL
-$("#ovr-brand").click(() => (window.location = "brand"));
-$("#ovr-music").click(() => (window.location = "music"));
-$("#ovr-animation").click(() => (window.location = "animation"));
-$("#ovr-about").click(() => (window.location = "about"));
-$("#ovr-contact").click(() => (window.location = "contact"));
+$("#ovr-brand").click(() => (window.location.href = "/brand"))
+$("#ovr-music").click(() => (window.location.href = "/music"))
+$("#ovr-animation").click(() => (window.location.href = "/animation"))
+$("#ovr-about").click(() => (window.location.href = "/about"))
+$("#ovr-contact").click(() => (window.location.href = "/contact"))
 
 // HOME
 $("#home-body").ripples({
   resolution: 512,
   dropRadius: 20,
   perturbance: 0.04
-});
+})
 
 //BRAND
