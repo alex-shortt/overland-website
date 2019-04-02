279ea3b3365d5050704955a35af7553f4e699c0a
Alex Shortt <alexander.shortt@gmail.com>
2019-04-02 16:39:59 -0700

    locally import jquery, add ripple effect

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import "./styles/main.scss";
+import "./components/jquery-3.3.1.min.js";
+import "./components/jquery.ripples.js";
 
-// HOME
+// GLOBAL
 $("#ovr-brand").click(() => (window.location = "brand"));
 $("#ovr-music").click(() => (window.location = "music"));
 $("#ovr-animation").click(() => (window.location = "animation"));
 $("#ovr-about").click(() => (window.location = "about"));
 $("#ovr-contact").click(() => (window.location = "contact"));
+
+// HOME
+$("#home-body").ripples({
+  resolution: 512,
+  dropRadius: 20,
+  perturbance: 0.04
+});
 
 //BRAND
