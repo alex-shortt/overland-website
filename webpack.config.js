9bbc4e2da18442b8393a8b22114e63f6112201d9
Alex Shortt <alexander.shortt@gmail.com>
2019-04-04 13:01:30 -0700

    move assets to root directory

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,9 @@ var webpack = require("webpack")
 
 module.exports = {
   entry: "./src/index.js",
+  resolve: {
+    modules: [path.resolve(__dirname, 'src'), 'node_modules']
+  },
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist")
