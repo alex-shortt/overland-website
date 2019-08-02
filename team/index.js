b0581faa6476a74d9a7a1f235ee0ccf6ffd0ac8c
Alex Shortt <alexander.shortt@gmail.com>
2019-08-01 18:37:19 -0700

    finish styles, pull info from definitions.js, masonry image grid

--- team/index.js
+++ team/index.js
@@ -1,4 +1,56 @@
 import "./styles/main.scss"
-import { members } from "./components/definitions"
+import { cloudfrontURL, members } from "./components/definitions"
 
-console.log(members)
+export function getHash() {
+  return location.hash.replace(/^#/, "") || ""
+}
+
+function shuffle(array) {
+  var currentIndex = array.length,
+    temporaryValue,
+    randomIndex
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex)
+    currentIndex -= 1
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex]
+    array[currentIndex] = array[randomIndex]
+    array[randomIndex] = temporaryValue
+  }
+
+  return array
+}
+
+const member = members[getHash()]
+if (!member) {
+  window.location.hash = ""
+  window.location.pathname = ""
+}
+
+function resolveImageURL(url) {
+  return `${cloudfrontURL}${url}`
+}
+
+$("#name").html(member.name)
+$("#desc").html(member.desc)
+$("#profile-image").attr("src", resolveImageURL(member.profileImage))
+shuffle(member.photos).map(photo =>
+  $(".mosaic-wrapper").append(`
+  <div class="mosaic-item">
+    <img src=${resolveImageURL(photo)} />
+  </div>
+`)
+)
+
+$(".mosaic-wrapper").masonry({
+  itemSelector: ".mosaic-item",
+  isAnimated: true
+})
+// // layout Masonry after each image loads
+// $grid.imagesLoaded().progress(function() {
+//   $grid.masonry("layout")
+// })
